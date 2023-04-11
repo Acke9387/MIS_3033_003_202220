69c2266d2a9ece80151081b172f4ecec62fca5a2
Ackerman, Adam P <adamackrmn@gmail.com>
2023-04-11 09:43:21 -0500

    Did an inclass example over MVC

---
+++ In_Class_Examples/MyFirstMVCApplication/Controllers/TAController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstMVCApplication.Controllers
+{
+    public class TAController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> tas = new List<string>();
+            tas.Add("Connor");
+            tas.Add("Matt");
+            tas.Add("Hailey");
+            tas.Add("Boone");
+
+            return View(tas);
+        }
+    }
+}
