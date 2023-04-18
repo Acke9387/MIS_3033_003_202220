a55ac71da900209174ce1f5c7cc7f6785eb43849
Ackerman, Adam P <adamackrmn@gmail.com>
2023-04-18 08:51:27 -0500

    changes

--- In_Class_Examples/MyFirstMVCApplication/Controllers/TAController.cs
+++ In_Class_Examples/MyFirstMVCApplication/Controllers/TAController.cs
@@ -14,5 +14,30 @@ namespace MyFirstMVCApplication.Controllers
 
             return View(tas);
         }
+
+        public IActionResult Info(string name = "")
+        {
+            List<string> tas = new List<string>();
+            tas.Add("Connor");
+            tas.Add("Matt");
+            tas.Add("Hailey");
+            tas.Add("Boone");
+
+            List<string> ta = tas.Where(x => x == name).ToList();
+
+            return View(ta);
+        }
+        public IActionResult Deets(string id)
+        {
+            List<string> tas = new List<string>();
+            tas.Add("Connor");
+            tas.Add("Matt");
+            tas.Add("Hailey");
+            tas.Add("Boone");
+
+            string ta = tas.Where(x => x == id).Single();
+
+            return View(ta);
+        }
     }
 }
